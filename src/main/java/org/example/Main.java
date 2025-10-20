b2e9ce7ee3c9d3de4855ab87010ad8552779f0ba
dominik.halvonik <dominik.halvonik@gmail.com>
2025-10-20 14:07:28 +0200

    Actions + JS

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -11,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\IdeaProjects\\chrome-drivers\\chromedriver-140.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://ais2.ukf.sk/ais/start.do");
+        /*driver.get("https://ais2.ukf.sk/ais/start.do");
         WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[6]/button[1]"));
         cookieButton.click();
 
@@ -39,11 +45,59 @@ public class Main {
         WebElement usernameCss = driver.findElement(By.cssSelector("input[class=login_field]"));
         //Vyhľadávanie na základe XPath
         WebElement usernameXpath = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div"));
+         */
 
+        driver.get("https://www.alza.sk/mobily/18843445.htm");
         try {
+            WebElement cookiebutton = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/a[1]"));
+            cookiebutton.click();
+            Thread.sleep(1000);
+
+            WebElement selectBox = driver.findElement(By.id("cpcm_cpc_parametrization_rptHierarchicParameterGroups_hierarchicParameterGroup_0_rptParameters_0_ddlParameterValues_0"));
+            Select dropdown = new Select(selectBox);
+
+            // Získame všetky možnosti v zozname
+            List<WebElement> options = dropdown.getOptions();
+            // Prejdeme všetky možnosti a vypíšeme ich text do konzoly
+            for (WebElement option : options) {
+                if(option.getText().equals("Apple")) {
+                    System.out.println("Ano selectBox obsahuje znacku Apple");
+                }
+                System.out.println("Option text: " + option.getText() + " " + "Option value: " + option.getAttribute("value"));
+            }
+
+            //dropdown.selectByIndex(5);
+            //dropdown.selectByValue("3933");
+            dropdown.selectByVisibleText("Apple");
+            Thread.sleep(5000);
+
+            Actions act = new Actions(driver);
+
+            act.sendKeys(Keys.ARROW_DOWN).perform();
+            Thread.sleep(1000);
+            act.sendKeys(Keys.PAGE_DOWN).perform();
+
+            Thread.sleep(5000);
+
+
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+
+            js.executeScript("confirm('Potvrd ale vyvrat')");
+            Thread.sleep(1000);
+            driver.switchTo().alert().dismiss();
+            Thread.sleep(1000);
+
+            js.executeScript("alert(22)");
+            Thread.sleep(1000);
+            driver.switchTo().alert().accept();
+
+            js.executeScript("alert('Ahoj ja som tu')");
+            Thread.sleep(1000);
+            System.out.println(driver.switchTo().alert().getText());
+            driver.switchTo().alert().accept();
             Thread.sleep(5000);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         } finally {
             driver.quit();
         }
